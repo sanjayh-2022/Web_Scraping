44144cc4f78a23cdfbbf8cb2595aa01ccf99bd62
Aswinr24 <135364633+Aswinr24@users.noreply.github.com>
2024-01-26 14:42:34 +0530

    Update scraping_divtext.py

--- scraping_divtext.py
+++ scraping_divtext.py
@@ -5,9 +5,9 @@ class webscrape:
     def content(self, url):
         request = requests.get(url)
         soup = BeautifulSoup(request.content, "html5lib")
-        for para in soup.find_all('div', class_="mw-content-ltr mw-parser-output"):
-            print(para.text)
+        for para in soup.find_all('div', attrs={'class':"description"}):
+             print(para.find('a')['title'], end='\n\n')
 
 if __name__ == "__main__":
     web_obj = webscrape()
-    web_obj.content("https://en.wikipedia.org/wiki/The_Avengers_(2012_film)")
+    web_obj.content("https://www.moneycontrol.com/news/")
